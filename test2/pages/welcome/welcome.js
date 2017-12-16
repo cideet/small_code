5184223774a6f6186ba99f6397750b163388b7fa
zhangsanfeng <123456@qq.com>
2017-12-16 15:12:33 +0800

    wx.navigateTo->onHide   wx.redirectTo->onUnload

--- test2/pages/welcome/welcome.js
+++ test2/pages/welcome/welcome.js
@@ -1,11 +1,12 @@
 Page({
 
   onTap: function () {
-    // 跳转带返回
+    // 跳转带返回，最多5级，对应的是onHide
     // wx.navigateTo({
     //   url: "../post/post"
     // });
 
+    // 对应的是onUnload
     wx.redirectTo({
       url: "../post/post"
     });
@@ -22,55 +23,55 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log('onLoad');
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    console.log('onReady');
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log('onShow');
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log('onHide');
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    console.log('onUnload');
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log('onPullDownRefresh');
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    console.log('onReachBottom');
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+    console.log('onShareAppMessage');
   }
 })
