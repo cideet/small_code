5b293d4d160a192843af604d34009781595a73a4
zhangsanfeng <123456@qq.com>
2017-12-16 14:49:58 +0800

    for循环： wx:for="{{post_content}}" wx:for-item="item" wx:for-index="index"

--- test2/pages/post/post.js
+++ test2/pages/post/post.js
@@ -12,16 +12,29 @@ Page({
    */
   onLoad: function (options) {
     console.log('onLoad');
-    var post_content1 = {
-      date: '2017-12-12',
-      title: 'title',
-      post_img: '/images/img2.jpg',
-      content: 'content',
-      view_num: '112',
-      collect_num: '88',
-      author_img: '/images/img3.jpg'
-    };
-    this.setData(post_content1);
+    var post_content = [
+      {
+        date: '2017-12-16',
+        title: 'title',
+        post_img: '/images/img2.jpg',
+        content: 'content',
+        view_num: '112',
+        collect_num: '88',
+        author_img: '/images/img2.jpg'
+      },
+      {
+        date: '2017-12-12',
+        title: 'title2',
+        post_img: '/images/img3.jpg',
+        content: 'content3',
+        view_num: '112',
+        collect_num: '88',
+        author_img: '/images/img3.jpg'
+      }
+    ];
+    this.setData({
+      post_content: post_content
+    });
   },
 
   /**
