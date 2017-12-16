e53bbfb309d03f3cd7cce77b779b64f0428fa5b4
zhangsanfeng <123456@qq.com>
2017-12-17 01:08:34 +0800

    playBackgroundAudio-音乐播放基本实现

--- test2/pages/post/post-detail/post-detail.js
+++ test2/pages/post/post-detail/post-detail.js
@@ -12,15 +12,19 @@ Page({
   },
 
   onMusicTap: function (event) {
+    var currentPostid = this.data.currentPostid;
     var isPlayingMusic = this.data.isPlayingMusic;
     if (isPlayingMusic) {
       wx.pauseBackgroundAudio();
       this.setData({ isPlayingMusic: false });
     } else {
       wx.playBackgroundAudio({
-        dataUrl: "http://ws.stream.qqmusic.qq.com/C100003507bR0gDKBm.m4a?fromtag=38",
-        title: "夜夜夜夜-齐秦",
-        coverImgUrl: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001TEc6V0kjpVC.jpg?max_age=2592000"
+        dataUrl: postsData.postList[currentPostid].music.url,
+        title: postsData.postList[currentPostid].music.title,
+        coverImgUrl: postsData.postList[currentPostid].music.coverImg
+        // dataUrl: "http://ws.stream.qqmusic.qq.com/C100003507bR0gDKBm.m4a?fromtag=38",
+        // title: "夜夜夜夜-齐秦",
+        // coverImgUrl: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001TEc6V0kjpVC.jpg?max_age=2592000"
       });
       this.setData({ isPlayingMusic: true });
     }
